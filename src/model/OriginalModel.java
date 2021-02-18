b42ab70846cbb44bcc83251f29e7d9ffc69f1768
Love <love.lindqvist101@gmail.com>
2021-02-19 00:51:01 +0100

    Fixed methods for checking Neighbour, that return data in form of proof. It ain't pretty, but it works
    Added TODO for how to fix Neighbour method

--- src/model/OriginalModel.java
+++ src/model/OriginalModel.java
@@ -20,7 +20,7 @@ public class OriginalModel implements IModel{
 
     public void createWorld(int size){
         this.size = size;
-        world = new ICell[size][size];
+        world = new Cell[size][size];
         int filledCells = 0;
         boolean lookForProvenCells = true;
         while (filledCells < size*size){
@@ -34,7 +34,6 @@ public class OriginalModel implements IModel{
     }
 
     private int fillRandomCell(){
-        int filledCells = 0;
         int row = 0;
         int col = 0;
         while (world[row][col].isFilled()){
@@ -42,32 +41,36 @@ public class OriginalModel implements IModel{
             col = random.nextInt(size);
         }
         if (random.nextBoolean()){
-            world[row][col].setRed();
+            world[row][col].setColor(State.RED);
         } else {
-            world[row][col].setBlue();
+            world[row][col].setColor(State.BLUE);
         }
         return 1;
     }
 
     private int fillAllProvenCells() {
+        int filledCells = 0;
+        Proof proof;
         for (int row = 0; row < size; row++){
             for (int col = 0; col < size; col++) {
                 if (world[row][col].isFilled()){
                     continue;
                 }
-                if (isProvable(row, col)){
-
-                }
+                proof = getProof(row, col);
+                world[row][col].setColor(proof.getColor());
+                row = 0;
+                col = 0;
+                filledCells++;
             }
         }
-        return 1;
+        return filledCells;
     }
 
     public boolean isCompleted(){
         return completed;
     }
 
-    public void changeCell(Position position){
+    public void nextState(Position position){
         cellAt(position).nextState();
         update();
     }
@@ -76,43 +79,107 @@ public class OriginalModel implements IModel{
         return world[position.row][position.column];
     }
 
-    private boolean isProvable(Position position){
-        if (provableByNeighbour(position)) return true;
-        if (provableOnRow(position)) return true;
-        if (provableOnCol(position)) return true;
-        if (provableByOrder(position)) return true;
-        if (provableByOddOneOut(position)) return true;
-        return false;
+    private Proof getProof(Position position){
+        Proof p = new Proof();
+        p.add(provableByNeighbour(position));
+        p.add(provableOnRow(position));
+        p.add(provableOnCol(position));
+        p.add(provableBySameRow(position));
+        p.add(provableByOddOneOut(position));
+        return p;
     }
 
-    private boolean isProvable(int row, int col){
-        return isProvable(new Position(row, col));
+    private Proof getProof(int row, int col){
+        return getProof(new Position(row, col));
+    }
+    //TODO Vastly simplify
+    //getting the 4 cells in a List, and checking if 2 or more of them are the same color
+    // (if 2 & 2, add RED and BLUE for INVALID)
+    private Proof provableByNeighbour(Position position){
+        Proof proof = new Proof();
+        if (position.row >= 2){
+            proof.add(readNeighbourAbove(position));
+        }
+        if (position.row > 0 && position.row < size-1){
+            proof.add(readOnHeight(position));
+        }
+        if (position.row < size-2){
+            proof.add(readNeighbourBelow(position));
+        }
+        if (position.column >=2){
+            proof.add(readNeighbourLeft(position));
+        }
+        if (position.column > 0 && position.column < size - 1){
+            proof.add(readOnWidth(position));
+        }
+        if (position.column < size -2){
+            proof.add(readNeighbourRight(position));
+        }
+        return proof;
     }
 
-    private boolean provableByNeighbour(Position position){return false;}
+    State readNeighbourAbove(Position position){
+        ICell cellAbove1 = world[position.row-1][position.column];
+        ICell cellAbove2 = world[position.row-2][position.column];
+        if (cellAbove1.equals(cellAbove2)){
+            return cellAbove1.getState();
+        }
+        return State.NONE;
+    }
 
-    private boolean provableOnRow(Position position){return false;}
+    State readOnHeight(Position position){
+        ICell cellAbove = world[position.row-1][position.column];
+        ICell cellBelow = world[position.row+1][position.column];
+        if (cellAbove.equals(cellBelow)){
+            return cellAbove.getState();
+        }
+        return State.NONE;
+    }
 
-    private boolean provableOnCol(Position position){return false;}
+    State readNeighbourBelow(Position position){
+        ICell cellBelow1 = world[position.row+1][position.column];
+        ICell cellBelow2 = world[position.row+2][position.column];
+        if (cellBelow1.equals(cellBelow2)){
+            return cellBelow1.getState();
+        }
+        return State.NONE;
+    }
 
-    private boolean provableByOrder(Position position){return false;}
+    State readNeighbourLeft(Position position){
+        ICell cellLeft1 = world[position.row][position.column-1];
+        ICell cellBLeft2 = world[position.row][position.column-2];
+        if (cellLeft1.equals(cellBLeft2)){
+            return cellLeft1.getState();
+        }
+        return State.NONE;
+    }
 
-    private boolean provableByOddOneOut(Position position){return false;}
+    State readOnWidth(Position position){
+        ICell cellLeft = world[position.row][position.column-1];
+        ICell cellRight = world[position.row][position.column+1];
+        if (cellLeft.equals(cellRight)){
+            return cellLeft.getState();
+        }
+        return State.NONE;
+    }
+
+    State readNeighbourRight(Position position){
+        ICell cellRight1 = world[position.row+1][position.column];
+        ICell cellRight2 = world[position.row+2][position.column];
+        if (cellRight1.equals(cellRight2)){
+            return cellRight1.getState();
+        }
+        return State.NONE;
+    }
+
+    private Proof provableOnRow(Position position){return new Proof();}
+
+    private Proof provableOnCol(Position position){return new Proof();}
+
+    private Proof provableBySameRow(Position position){return new Proof();}
+
+    private Proof provableByOddOneOut(Position position){return new Proof();}
 
     private void removeCell(Position position){}
 
-    private List<Position> getRemovableCellsPosition(){
-        Position position = new Position();
-        List<Position> removables = new ArrayList<>();
-        for (int row = 0; row < size; row++){
-            position.row = row;
-            for (int col = 0; col < size; col++){
-                position.column = col;
-                if ( isProvable( position ) ){
-                    removables.add( position );
-                }
-            }
-        }
-        return removables;
-    }
 }
