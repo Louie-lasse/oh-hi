c13828765ef6d903d0d02846c05c6ddcb7b96580
Love <love.lindqvist101@gmail.com>
2021-02-22 22:29:50 +0100

    Moved application class to Main class and added methods for displaying the world in text

---
+++ src/Application/Main.java
@@ -0,0 +1,34 @@
+package Application;
+
+import model.ICell;
+import model.IModel;
+import model.OriginalModel;
+import model.State;
+
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        IModel model = new OriginalModel();
+        model.createWorld(4);
+        displayWorld(model.getWorld());
+    }
+
+    public static void displayWorld(ICell[][] world){
+        for (ICell[] row: world){
+            displayRow(row);
+            out.println();
+        }
+        out.println("\n");
+    }
+
+    public static void displayRow(ICell[] row){
+        for (ICell cell: row){
+            out.print(cell.getState());
+            out.print(", ");
+            if (cell.getState() == State.RED){
+                out.print(" ");
+            }
+        }
+    }
+}
